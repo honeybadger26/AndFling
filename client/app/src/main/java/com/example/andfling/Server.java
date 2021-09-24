d26a84887b7a612f5c8e0f2cb7491757211146fc
honeybadger26 <honeybadger26@gmail.com>
2021-09-24 23:17:29 +1000

    uploading files

--- client/app/src/main/java/com/example/andfling/Server.java
+++ client/app/src/main/java/com/example/andfling/Server.java
@@ -3,15 +3,23 @@ package com.example.andfling;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.os.Environment;
 import android.text.format.Formatter;
+
+import androidx.annotation.RequiresApi;
 
 import com.example.andfling.database.AppDatabase;
 import com.example.andfling.database.Message;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import fi.iki.elonen.NanoHTTPD;
 
@@ -37,38 +45,66 @@ public class Server extends NanoHTTPD {
         body = newBody.toString();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     public Response serve(IHTTPSession session) {
         Method method = session.getMethod();
 
         if (Method.POST.equals(method)) {
-            Map<String, String> request = new HashMap<String, String>();
+            Map<String, String> request = new HashMap<>();
+
             try {
                 session.parseBody(request);
-                Map<String, List<String>> params = session.getParameters();
-                Message msg = new Message();
-                msg.date = "now";
-                msg.contents  = params.get("message").get(0);
-                msg.roomId = 1;
-                db.messageDao().insertAll(msg);
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (ResponseException e) {
                 e.printStackTrace();
             }
+
+            Map<String, List<String>> params = session.getParameters();
+            Message msg = new Message();
+            msg.date = "now";
+            msg.roomId = 1;
+
+            if (params.containsKey("message")) {
+                msg.contents = params.get("message").get(0);
+            } else if (params.containsKey("file")) {
+                msg.contents  = params.get("file").get(0);
+
+                File inFile = new File(Objects.requireNonNull(request.get("file")));
+                String downloadsDir = String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS));
+                String outFilePath = downloadsDir + "/" + new Date().getTime() + "_" + msg.contents;
+                File outFile = new File(outFilePath);
+                try {
+                    Files.copy(inFile.toPath(), outFile.toPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                msg.contents = "INVALID MESSAGE";
+            }
+
+            db.messageDao().insertAll(msg);
         }
 
         String formComponent =
-            "<form action='' method='post'>" +
-            "   <input type='text' name='message'>" +
-            "   <input type='submit' name='submit' value='Send'>" +
-            "</form>";
+            "<div>" +
+            "  <form action='' method='post'>" +
+            "     <input type='text' name='message'>" +
+            "     <input type='submit' name='submit' value='Send'>" +
+            "  </form>" +
+            "</div>" +
+            "<div>" +
+            "  <form action='' method='post' enctype='multipart/form-data'>" +
+            "     <input type='file' name='file'>" +
+            "     <input type='submit' name='submit' value='Upload'>" +
+            "  </form>" +
+            "</div>";
 
         return newFixedLengthResponse(
-      "<html><body>" +
-                body +
-                formComponent +
-            "</body></html>"
+      "<html>" +
+            "  <body>" + body + formComponent + "</body>" +
+            "</html>"
         );
     }
 
